2a7fd30b532265bba2839ae10ca5806d4ba15273
Andrea Amorosi <dreamorosi@gmail.com>
2023-09-28 17:15:34 +0200

    Revert "feat(idempotency): add idempotency decorator (#1723)"
    This reverts commit d138673a33ff31f40b225dc046b2ff8258d0a97d.

--- packages/idempotency/src/index.ts
+++ packages/idempotency/src/index.ts
@@ -1,5 +1,4 @@
 export * from './errors';
 export * from './IdempotencyConfig';
 export * from './makeIdempotent';
-export * from './idempotencyDecorator';
 export { IdempotencyRecordStatus } from './constants';
